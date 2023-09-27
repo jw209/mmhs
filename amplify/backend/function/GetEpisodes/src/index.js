d376b4e8b7c5ec435b5c1fb6c509a712ad61bf8d
Jesse <77357219+jw209@users.noreply.github.com>
2023-09-27 18:30:49 -0400

    Change api

--- amplify/backend/function/GetEpisodes/src/index.js
+++ amplify/backend/function/GetEpisodes/src/index.js
@@ -1,17 +1,27 @@
+let Parser = require('rss-parser');
+let parser = new Parser();
 
- 
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
+
+    let feed = await parser.parseURL('https://media.rss.com/matchup-mashup/feed.xml');
+    console.log(feed.title);
+
+    let objects;
+
+    feed.items.forEach(item => {
+        objects = {
+            title: item.title,
+            link: item.link
+        }
+    });
+
     return {
         statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*"
+        },
+        body: JSON.stringify(objects),
     };
 };
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,26 @@
 import { Layout } from 'antd';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { API } from 'aws-amplify';
+
+const myAPI = "podcasts";
+const path = '/episodes';
 
 function HomePage() {
+    //Function to fetch from our backend and update customers array
+    function getStuff() {
+        API.get(myAPI, path)
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
+    useEffect(() => {
+        getStuff();
+    }, [])
+
     return (
         <Layout style={{ 
             flex: 1,
