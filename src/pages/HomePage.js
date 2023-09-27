1e2ebb4aa5f4b16f089f8336bdebf00a593475c9
Jesse <77357219+jw209@users.noreply.github.com>
2023-09-27 16:46:37 -0400

    Remove api call from home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,31 +1,7 @@
 import { Layout } from 'antd';
-import { API } from 'aws-amplify';
-import React, { useState, useEffect } from 'react';
-
-const myAPI = "episodes";
-const path = "/episodes"; 
+import React from 'react';
 
 function HomePage() {
-    const [episodes, setEpisodes] = useState([])
-
-    //Function to fetch from our backend and update customers array
-    function getEpisodes() {
-        API.get(myAPI, path + "/" + "episode")
-        .then(response => {
-            console.log(response)
-            let newEpisodes = [...episodes]
-            newEpisodes.push(response)
-            setEpisodes(newEpisodes)
-        })
-        .catch(error => {
-            console.log(error)
-        })
-    }
-
-    useEffect(() => {
-        getEpisodes()
-    }, [])
-
     return (
         <Layout style={{ 
             flex: 1,
