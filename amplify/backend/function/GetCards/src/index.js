77538b724342d25153b8b263959aab6b64847845
Jesse <77357219+jw209@users.noreply.github.com>
2023-10-08 23:27:58 -0400

    Add an api for getting cards

--- amplify/backend/function/GetCards/src/index.js
+++ amplify/backend/function/GetCards/src/index.js
@@ -1,17 +1,51 @@
+const https = require('https');
+const BLIZZARD_API_URL = 'api.blizzard.com';
+const API_KEY = process.env.CLIENT_ID;
+const API_SECRET = process.env.SECRET_CLIENT_ID;
 
-
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+  const cardName = event.cardName; // Retrieve card name from the Lambda event
+
+  // Define API request options
+  const options = {
+    hostname: BLIZZARD_API_URL,
+    path: `/hearthstone/cards?name=${encodeURIComponent(cardName)}`,
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${API_KEY}:${API_SECRET}`,
+    },
+  };
+
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        if (res.statusCode === 200) {
+          resolve({
+            statusCode: 200,
+            body: data,
+          });
+        } else {
+          reject({
+            statusCode: res.statusCode,
+            body: JSON.stringify({ error: 'An error occurred' }),
+          });
+        }
+      });
+    });
+
+    req.on('error', (error) => {
+      reject({
+        statusCode: 500,
+        body: JSON.stringify({ error: 'An error occurred' }),
+      });
+    });
+
+    req.end();
+  });
 };
--- src/pages/DecksPage.js
+++ src/pages/DecksPage.js
@@ -1,8 +1,26 @@
 import { Layout, Spin } from 'antd';
 import DeckList from '../components/DeckList'
 import React, { useState } from 'react'
+
+const myAPI = 'cardsapi'
+const path = '/cardsapi'
+
 function DecksPage() {
   const [loadingDeckLists, setLoadingDeckLists] = useState(true);
+  const [cards, setCards] = useState({})
+
+  function getCards() {
+    API.get(myAPI, path)
+    .then(response => {
+      setEpisodes(response)
+      setLoading(false);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  
 
   const handleLoadingStateChangeList = (updatedState) => {
     setLoadingDeckLists(updatedState)
